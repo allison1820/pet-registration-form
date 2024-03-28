9c35db7ba1ebd122bdfc4643badf57f9d44606b9
allison1820 <awilliams76@student.cccd.edu>
2024-03-27 19:45:23 -0700

    validateForm

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ function validateForm() {
     // TODO: Add checks here.
 
   const form = document.forms['pet-info-form'];
-  
+
+    
   const bordetella = form['bordetella'].checked;
   const dhlpp = form['dhlpp'].checked;
   const rabies = form['rabies'].checked;
 
-  if(!(bordetella&&dhlpp&&rabies))
+  if(!(bordetella&&dhlpp&&rabies) && (document.getElementById('other-info-text-area').value != '')){
+    document.getElementById('vaccs').style.color = 'red'
     return false;
+  }
+
+
 
   return true;
 }

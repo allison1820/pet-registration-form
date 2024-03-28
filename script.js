c9af10c6d3060fe7ec371493a6be22f7e5ce58e4
allison1820 <awilliams76@student.cccd.edu>
2024-03-27 19:10:15 -0700

    TODOs

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function validateForm() {
+    // TODO: Add checks here.
+
   const form = document.forms['pet-info-form'];
-  // TODO: Add checks here.
+  
+  const bordetella = form['bordetella'].checked;
+  const dhlpp = form['dhlpp'].checked;
+  const rabies = form['rabies'].checked;
+
+  if(!(bordetella&&dhlpp&&rabies))
+    return false;
+
   return true;
 }
